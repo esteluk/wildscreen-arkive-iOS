1f8396047005d0ce77ee38278533f206d272bf6d
Nathan Wong <nathan@nathanwong.co.uk>
2015-08-14 11:22:07 +0100

    Initial Commit

---
+++ Wildscreen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Wildscreen
+//
+//  Created by Nathan Wong on 14/08/2015.
+//  Copyright (c) 2015 Rockpool Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
